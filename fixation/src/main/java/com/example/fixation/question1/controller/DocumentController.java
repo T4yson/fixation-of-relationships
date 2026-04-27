0fae4964e723ffdc1f5615089edef13e1b274314
IGOR TAYSON BRESOLIN SAVERO <igor_savero@estudante.sesisenai.org.br>
2026-04-27 16:09:09 -0300

    feat: finish the controller package

--- fixation/src/main/java/com/example/fixation/question1/controller/DocumentController.java
+++ fixation/src/main/java/com/example/fixation/question1/controller/DocumentController.java
@@ -1,4 +1,26 @@
 package com.example.fixation.question1.controller;
 
+import com.example.fixation.question1.model.Document;
+import com.example.fixation.question1.service.DocumentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/documents")
+@RequiredArgsConstructor
 public class DocumentController {
+
+    private final DocumentService documentService;
+
+    @PostMapping
+    public ResponseEntity<Document> create(@RequestBody @Valid Document document) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(documentService.save(document));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Document> get(@PathVariable Long id) {
+        return ResponseEntity.ok(documentService.findById(id));
+    }
 }
--- fixation/src/main/java/com/example/fixation/question1/controller/PersonController.java
+++ fixation/src/main/java/com/example/fixation/question1/controller/PersonController.java
@@ -1,4 +1,26 @@
 package com.example.fixation.question1.controller;
 
+import com.example.fixation.question1.model.Person;
+import com.example.fixation.question1.service.PersonService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/persons")
+@RequiredArgsConstructor
 public class PersonController {
+
+    private final PersonService personService;
+
+    @PostMapping
+    public ResponseEntity<Person> create(@RequestBody Person person) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(personService.save(person));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(personService.findById(id));
+    }
 }
